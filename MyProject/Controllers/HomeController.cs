83a9b06385c1a86e8f51084edb70ffba5311f817
quangvinh93hcm <quangvinh050293@gmail.com>
2015-06-17 12:05:29 +0700

    abc

--- MyProject/Controllers/HomeController.cs
+++ MyProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            
 
             return View();
         }
